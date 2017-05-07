c8a0d49ca994dd728d76414f24dcc52edf2d3ac4
Nick Rupp <nickrupp95@gmail.com>
2017-05-07 23:43:41 +0100

    Add some awful styling

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,5 @@
 # config/initializers/paperclip.rb
 Paperclip::Attachment.default_options[:url] = 'pictoon.s3.amazonaws.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-west-2.amazonaws.com'
